1e0b88a329984b415efed056da3886a787539119
coder <110666273+coderBane@users.noreply.github.com>
2022-08-09 08:32:16 +0100

    Repository pattern implementation

---
+++ Foody/Foody.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Foody.Data.Data;
+using Foody.Entities.Models;
+using Foody.Data.Interfaces;
+
+namespace Foody.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(FoodyDbContext context) : base(context)
+        { 
+        }
+
+        public bool Exists(int id)
+        {
+            return (_context.Products?.Any(p => p.Id == id)).GetValueOrDefault();
+        }
+
+        public Task Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
