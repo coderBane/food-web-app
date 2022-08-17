07dca09a579be6460fc5a90f4b339230a08a8437
coder <110666273+coderBane@users.noreply.github.com>
2022-08-17 20:17:41 +0100

    mapping configuration

---
+++ Foody/Foody.WebApi/Profiles/ItemProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Foody.Entities.DTOs;
+using Foody.Entities.Models;
+
+namespace Foody.WebApi.Profiles
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<Category, CategoryDto>()
+                .ReverseMap();
+                
+            CreateMap<Category, CategoryDetailDto>();
+
+        }
+    }
+}
+
