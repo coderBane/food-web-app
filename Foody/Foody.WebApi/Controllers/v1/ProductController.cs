af8db0935a423d575ee265432ca49d469f36b1cc
coder <110666273+coderBane@users.noreply.github.com>
2022-08-21 01:06:31 +0100

    product controller

---
+++ Foody/Foody.WebApi/Controllers/v1/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using Foody.Entities.DTOs;
+using Foody.Entities.Models;
+using Foody.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Foody.WebApi.Controllers.v1
+{
+    public class ProductController : BaseController
+    {
+        public ProductController(IUnitofWork unitofWork, IMapper mapper) : base(unitofWork, mapper)
+        {
+        }
+
+        // GET: v1/Product
+        [HttpGet]
+        [HttpHead]
+        [ProducesResponseType(200, Type = typeof(ProductDto))]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _unitofWork.Products.All();
+            var _dto = _mapper.Map<List<ProductDto>>(products);
+            return products is not null ? Ok(_dto) :
+                Problem("Entity set 'FoodyDbContext' is null.");
+        }
+
+        // GET v1/Product/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(ProductDetailDto))]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitofWork.Products.Get(id);
+            var _dto = _mapper.Map<ProductDetailDto>(result);
+
+            return result is not null ? Ok(_dto) : NotFound();
+        }
+
+        // POST v1/Product
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(ProductDetailDto))]
+        public async Task<IActionResult> Post([FromForm] ProductModDto productModDto)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(productModDto);
+                await Upload(product, productModDto.ImageUpload);
+
+                ModelState.ClearValidationState(nameof(productModDto));
+                if (!TryValidateModel(product))
+                    return UnprocessableEntity(ModelState);
+
+                _unitofWork.Products.Add(product);
+                await _unitofWork.CompleteAsync();
+
+                var _dto = _mapper.Map<ProductDetailDto>(product);
+                return CreatedAtAction(nameof(Get), new { id = product.Id }, _dto);
+            }
+            catch (Exception ex) { return BadRequest(ex.ToString()); }
+        }
+
+        // PUT v1/Product/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> Put(int id, [FromForm] ProductModDto productModDto)
+        {
+            var product = await _unitofWork.Products.Get(id);
+
+            if (product is null)
+                return NotFound();
+
+            try
+            {
+                _mapper.Map(productModDto, product);
+                await Upload(product, productModDto.ImageUpload);
+
+                ModelState.ClearValidationState(nameof(productModDto));
+                if (!TryValidateModel(product))
+                    return UnprocessableEntity(ModelState);
+
+                await _unitofWork.Products.Update(product);
+                await _unitofWork.CompleteAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!_unitofWork.Products.Exists(id))
+            {
+                return NotFound();
+            }
+            catch (Exception ex) { return BadRequest(ex.ToString()); }
+
+            return NoContent();
+        }
+
+        // DELETE v1/Product/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _unitofWork.Products.Delete(id);
+                await _unitofWork.CompleteAsync();
+            }
+            catch (NullReferenceException) { return NotFound(); }
+            catch (Exception) { return BadRequest(); }
+
+            return NoContent();
+        }
+    }
+}
