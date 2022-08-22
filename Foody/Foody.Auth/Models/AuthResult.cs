5d3162c948b96b2d88b30c9101f94a11443fb306
coder <110666273+coderBane@users.noreply.github.com>
2022-08-22 01:52:47 +0100

    added jwt authentication

---
+++ Foody/Foody.Auth/Models/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Foody.Auth.Models;
+
+public class AuthResult
+{
+    public string? Token { get; set; }
+
+    public bool Success { get; set; }
+    
+    public IEnumerable<string> Errors { get; set; }
+
+    public AuthResult() => Errors = Enumerable.Empty<string>();
+}
