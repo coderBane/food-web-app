c855992cf712dbc16843c37ae33ae3cd54c1e2ec
coder <110666273+coderBane@users.noreply.github.com>
2022-09-04 09:09:39 +0100

    global usings

---
+++ src/client/FoodyApp/Foody.Admin/GlobalUsings.cs
@@ -0,0 +1,24 @@
+﻿#region imports
+
+#region System
+global using System.Text;
+global using System.Text.Json;
+global using System.Diagnostics;
+global using System.Net.Http.Json;
+global using System.Collections.ObjectModel;
+#endregion
+
+#region Foody
+global using Foody.Admin.Models;
+global using Foody.Admin.Manager;
+global using Foody.Admin.Utilities;
+global using Foody.Admin.ViewModels;
+global using Foody.Admin.Rest.Interfaces;
+#endregion
+
+#region Mvvm
+global using Microsoft.Toolkit.Mvvm.Input;
+#endregion
+
+#endregion
+
