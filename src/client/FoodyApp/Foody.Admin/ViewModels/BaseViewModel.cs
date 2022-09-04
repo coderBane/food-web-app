50410485d908a7f3a138947f4414faac9a99e2a4
coder <110666273+coderBane@users.noreply.github.com>
2022-09-04 09:09:06 +0100

    service app interaction

---
+++ src/client/FoodyApp/Foody.Admin/ViewModels/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Foody.Admin.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    protected readonly IDataManager dataManager;
+    //protected readonly IConnectivity connectivity;
+
+    public BaseViewModel(IDataManager dataManager)
+    {
+        this.dataManager = dataManager;
+    }
+
+    public bool IsNotBusy => !IsBusy;
+}
+
