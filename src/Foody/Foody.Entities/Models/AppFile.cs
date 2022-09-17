8765921c792a0945e28ce5c62d80cd0542801107
coder <110666273+coderBane@users.noreply.github.com>
2022-09-17 15:13:12 +0100

    add and refactor image storage model

---
+++ src/Foody/Foody.Entities/Models/AppFile.cs
@@ -0,0 +1,19 @@
+﻿namespace Foody.Entities.Models
+{
+    public sealed class AppFile : BaseEntity
+    {
+        [MaxLength(1048576, ErrorMessage = "Max upload size is 2MB.")]
+        public byte[] Content { get; set; } = null!;
+
+        public string UntrustedName { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? Description { get; set; }
+
+        [FileExtensions(Extensions = "png,jpg,jpeg,svg")]
+        public string FileExtension { get; set; } = null!;
+
+        public decimal Size { get; set; }
+    }
+}
+
