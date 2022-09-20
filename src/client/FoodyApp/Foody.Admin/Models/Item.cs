a4f234cc0c2faa422a66b4c62607e69f37829a3a
coder <110666273+coderBane@users.noreply.github.com>
2022-09-20 07:19:11 +0100

    image fix

--- src/client/FoodyApp/Foody.Admin/Models/Item.cs
+++ src/client/FoodyApp/Foody.Admin/Models/Item.cs
@@ -2,6 +2,15 @@
 
 namespace Foody.Admin.Models;
 
+public class Image
+{
+    public int Id { get; set; }
+
+    public long FileSize { get; set; }
+
+    public string Content { get; set; }
+}
+
 public class Item : Base
 {
     public int Id { get; set; }
@@ -17,10 +26,17 @@ public class CategoryDetail : Item
 {
     public string ImageUri { get; set; }
 
-    public byte[] ImageData { get; set; }
+    public Image Image { get; set; }
 
     public DateTime AddedOn { get; set; }
 
     public DateTime Updated { get; set; }
+}
+
+public class CategoryMod
+{
+    public string Name { get; set; }
+
+    public bool IsActive { get; set; }
 }
 
