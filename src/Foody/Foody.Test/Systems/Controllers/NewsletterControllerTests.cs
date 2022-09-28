b848ede6f85e4eb3faa26c5f6eaed98803226f57
coder <110666273+coderBane@users.noreply.github.com>
2022-09-29 00:51:28 +0100

    add newsletter initial controller tests

---
+++ src/Foody/Foody.Test/Systems/Controllers/NewsletterControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Foody.Test.Fixtures;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.Test.Systems.Controllers
+{
+    public class NewsletterControllerTests : TestBase
+    {   
+        [Fact]
+        public async void Post_Return_NoContent()
+        {
+            //Arrange
+            var subs = Subcribers.GetSubcribers();
+            A.CallTo(() => unitofWork.Subcribers.All(string.Empty)).Returns(Task.FromResult(subs));
+            var controller = new NewsletterController(unitofWork, mapper);
+            var fakeSub = A.Dummy<NewsletterDto>();
+            fakeSub.Email = subs.First().Email;
+            fakeSub.Name = subs.First().Name;
+
+            //Act
+            var actionResult = await controller.Post(fakeSub);
+
+            //Assert
+            var result = Assert.IsType<NoContentResult>(actionResult);
+        }
+    }
+}
+
