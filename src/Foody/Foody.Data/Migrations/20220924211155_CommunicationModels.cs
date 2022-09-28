786ca47844c4de8f0ef9a3307ae575c50034807e
coder <110666273+coderBane@users.noreply.github.com>
2022-09-29 00:36:44 +0100

    add newsletter and contact models

---
+++ src/Foody/Foody.Data/Migrations/20220924211155_CommunicationModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Foody.Data.Migrations
+{
+    public partial class CommunicationModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "inquiries",
+                schema: "api",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    tel = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    subject = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    message = table.Column<string>(type: "text", nullable: true),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_inquiries", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "subcribers",
+                schema: "api",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_subcribers", x => x.id);
+                },
+                comment: "Newsletter subcribers.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inquiries",
+                schema: "api");
+
+            migrationBuilder.DropTable(
+                name: "subcribers",
+                schema: "api");
+        }
+    }
+}
---
+++ src/Foody/Foody.Entities/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿namespace Foody.Entities.Models;
+
+public sealed record Contact
+{
+    [Key]
+    public int Id { get; init; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; init; } = null!;
+
+    [Phone]
+    public string? Tel { get; init; }
+
+    [Required]
+    [EmailAddress]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; init; } = null!;
+
+    [Required]
+    [StringLength(100, MinimumLength = 10)]
+    public string Subject { get; init; } = null!;
+
+    [DataType(DataType.MultilineText)]
+    public string? Message { get; init; } = null!;
+
+    [ScaffoldColumn(false)]
+    public DateTime Date { get; init; } = DateTime.UtcNow; 
+}
+
