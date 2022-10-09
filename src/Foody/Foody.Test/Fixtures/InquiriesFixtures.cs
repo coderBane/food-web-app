7e60af151114b0171da0b69265fc2fa69bc1d669
coder <110666273+coderBane@users.noreply.github.com>
2022-10-09 16:21:20 +0100

    configuring tests

---
+++ src/Foody/Foody.Test/Fixtures/InquiriesFixtures.cs
@@ -0,0 +1,31 @@
+﻿namespace Foody.Test.Fixtures
+{
+    public static class InquiriesFixtures
+    {
+        public static List<Contact> GetContacts() =>
+            Enumerable.Range(211, 218).Select(_ =>
+            {
+                var name = Faker.Name.FullName();
+
+                return new Contact
+                {
+                    Id = _,
+                    Name = name,
+                    Email = Faker.Internet.Email(name),
+                    Subject = Faker.Lorem.Sentence(10),
+                    Message = string.Join(Environment.NewLine, Faker.Lorem.Sentences(8)),
+                    Date = Faker.Identification.DateOfBirth()
+                };
+            }).ToList();
+
+        public static ContactDto ValidContact() =>
+            new()
+            {
+                Name = "Steve Mana",
+                Email = Faker.Internet.Email("Steve Mana"),
+                Subject = Faker.Lorem.Sentence(5),
+                Message = string.Join(Environment.NewLine, Faker.Lorem.Sentences(8))
+            };
+    }
+}
+
---
+++ src/Foody/Foody.Test/Helpers/ObjectValidator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Foody.Test.Helpers
+{
+    internal sealed class ObjectValidator : IObjectModelValidator
+    {
+        public void Validate(ActionContext actionContext, ValidationStateDictionary? validationState, string prefix, object? model)
+        {
+            var results = new List<ValidationResult>();
+
+            var context = new ValidationContext(model, null, null);
+
+            bool isValid = Validator.TryValidateObject(model, context, results, true);
+
+            if (!isValid)
+            {
+                results.ForEach(err
+                    => actionContext.ModelState.AddModelError(err.MemberNames.First() ?? string.Empty, err.ErrorMessage));
+            }
+        }
+    }
+}
+
