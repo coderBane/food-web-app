0c409918d35f96d8291bea106def49b482b4c59e
coder <110666273+coderBane@users.noreply.github.com>
2022-10-10 23:33:52 +0100

    add action filters
    - Validation attributes for model state validationState
    -  Exception filter to handle exceptions

---
+++ src/Foody/Foody.WebApi/Filters/ValidationFilter.cs
@@ -0,0 +1,49 @@
+﻿using Foody.WebApi.Controllers.v1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Foody.WebApi.Filters;
+
+public class ValidateModelAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            dynamic result;
+
+            var errors = context.ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(v => v.ErrorMessage).AsParallel().ToList();
+
+            if (context.Controller.GetType().IsSubclassOf(typeof(AuthBaseController)))
+            {
+                result = new AuthResult
+                {
+                    Success = false,
+                    Errors = errors
+                };
+            }
+            else
+            {
+                result = new Result<dynamic>
+                {
+                    Error = new()
+                    {
+                        Code = StatusCodes.Status400BadRequest,
+                        Title = ErrorsMessage.Generic.ValidationError,
+                        Message = errors.Aggregate((k, v) => $"{k} {Environment.NewLine} {v}")
+                    }
+                };
+            }
+
+            context.Result = new BadRequestObjectResult(result);
+        }
+    }
+
+    public override void OnActionExecuted(ActionExecutedContext context)
+    {
+
+    }
+}
+
