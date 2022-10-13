953b2e26054c09ab3ce9326b242037077383ca67
coder <110666273+coderBane@users.noreply.github.com>
2022-10-14 00:35:31 +0100

    filter configurations

--- src/Foody/Foody.WebApi/Extensions/Services/ApiServiceExtension.cs
+++ src/Foody/Foody.WebApi/Extensions/Services/ApiServiceExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foody.WebApi.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 namespace Foody.WebApi.Extensions.Services;
 
@@ -6,17 +8,29 @@ public class ApiServiceExtension : IServiceExtension
 {
     public void RenderService(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddControllers();
-
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
 
         services.AddApiVersioning(options =>
         {
             options.ReportApiVersions = true;
             options.AssumeDefaultVersionWhenUnspecified = true;
             options.DefaultApiVersion = ApiVersion.Default;
+        });
+
+        services.AddControllers(options =>
+        {
+            //options.Filters.Add(new NullableAttribute());
+            options.Filters.Add<ValidateModelAttribute>();
+        }).AddJsonOptions(option =>
+        {
+            //option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
         });
     }
 }
