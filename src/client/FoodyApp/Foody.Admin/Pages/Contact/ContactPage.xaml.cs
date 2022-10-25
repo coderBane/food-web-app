90a231ed9420332d35a299514ee6a512c2af2342
coder <110666273+coderBane@users.noreply.github.com>
2022-10-25 16:49:58 +0100

    add contact page

---
+++ src/client/FoodyApp/Foody.Admin/Pages/Contact/ContactPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Foody.Admin.ViewModels.Contact;
+
+namespace Foody.Admin.Pages.Contact;
+
+public partial class ContactPage : ContentPage
+{
+	public ContactPage(ContactVM viewmodel)
+	{
+		InitializeComponent();
+		BindingContext = viewmodel;
+	}
+}
