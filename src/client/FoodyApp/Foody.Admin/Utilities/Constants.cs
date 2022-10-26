fde2ab6b05a18412d4bb6b3b010ef26943fa86b4
coder <110666273+coderBane@users.noreply.github.com>
2022-10-26 15:52:38 +0100

    add constants class

---
+++ src/client/FoodyApp/Foody.Admin/Utilities/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Foody.Admin.Utilities;
+
+public static class Constants
+{
+    public const string jwt = "jwt_token";
+    public const string refresh = "refresh_token";
+    public const string tryAgain = "Try Again";
+    public const string authFail = "Authentication Failed";
+}
+
