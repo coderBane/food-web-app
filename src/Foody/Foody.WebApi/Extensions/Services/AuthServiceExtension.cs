7dd68a491ecec66b75b0e7a4afb998a442f282e9
coder <110666273+coderBane@users.noreply.github.com>
2023-07-18 21:11:05 +0100

    edit - modify token clock skew

--- src/Foody/Foody.WebApi/Extensions/Services/AuthServiceExtension.cs
+++ src/Foody/Foody.WebApi/Extensions/Services/AuthServiceExtension.cs
@@ -4,7 +4,8 @@ using Foody.WebApi.Extensions.Services.Base;
 
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-
+using Microsoft.AspNetCore.Identity;
+using Foody.Data.Data;
 
 namespace Foody.WebApi.Extensions.Services;
 
@@ -15,7 +16,6 @@ public class AuthServiceExtension : IServiceExtension
         // Update JWT class
         services.Configure<JwtConfig>(configuration.GetSection("JwtConfig"));
 
-        // Add TokenValidationParameters to DI
         var tokenValidationParams = new TokenValidationParameters
         {
             ValidateIssuer = false, // TODO Update to true
@@ -25,9 +25,15 @@ public class AuthServiceExtension : IServiceExtension
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.ASCII.GetBytes(configuration["JwtConfig:Secret"])),
+            ClockSkew = TimeSpan.FromSeconds(5)
         };
 
+        // Add TokenValidationParameters to DI
         services.AddSingleton(tokenValidationParams);
+
+        // Add Identity DI
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FoodyDbContext>();
 
         // Add jwt authentication
         services.AddAuthentication(options =>
