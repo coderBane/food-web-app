2e31bc05d2ed97161db7afecf92fe406ec12e557
coder <110666273+coderBane@users.noreply.github.com>
2023-07-18 20:46:58 +0100

    edit - handle cache exceptions

--- src/Foody/Foody.Data/Services/CacheService.cs
+++ src/Foody/Foody.Data/Services/CacheService.cs
@@ -17,17 +17,18 @@ public sealed class CacheService : ICacheService
 
     public async Task<T?> GetData<T>(string key)
     {
+        if (!_cachedB.IsConnected(key)) return default;
         var value = await _cachedB.StringGetAsync(key);
         return !string.IsNullOrEmpty(value) ? JsonSerializer.Deserialize<T>(value!) : default;
     }
 
     public async Task<bool> SetData<T>(string key, T value, DateTimeOffset expirationTime)
     {
+        if (!_cachedB.IsConnected(key)) return false;
         var expire = expirationTime.DateTime.Subtract(DateTime.Now);
-        return await _cachedB.StringSetAsync(key, JsonSerializer.Serialize<T>(value), expire);
+        return await _cachedB.StringSetAsync(key, JsonSerializer.Serialize(value), expire);
     }
 
-    public async Task<bool> RemoveData(string key) =>
-        await _cachedB.KeyExistsAsync(key) ? await _cachedB.KeyDeleteAsync(key) : false;
+    public async Task<bool> RemoveData(string key) => _cachedB.IsConnected(key) && await _cachedB.KeyDeleteAsync(key);
 }
 
